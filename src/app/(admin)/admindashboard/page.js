bc0c3dfb739cc77ef24a8bc15e2fe1364403a4af
developercodingarrow <digitalcloudinfo@gmail.com>
2024-10-13 02:15:29 +0530

    admindashbord stats correct

--- src/app/(admin)/admindashboard/page.js
+++ src/app/(admin)/admindashboard/page.js
@@ -3,6 +3,7 @@ import Webstats from "./webstats";
 import Loading from "./loading";
 import { cookies } from "next/headers";
 import { webStatsAction } from "./apiActions";
+
 export default async function DashBordpage() {
   const cookieStore = cookies();
   const authToken = cookieStore.get("jwt")?.value; // Get the auth token from cookies
@@ -10,7 +11,13 @@ export default async function DashBordpage() {
   let webStats; // Declare variable to hold web stats
   try {
     // Fetch the web stats using the auth token
-    webStats = await webStatsAction(authToken); // Pass the token to the action
+    const res = await webStatsAction(authToken); // Pass the token to the action
+    console.log(res);
+    if (res.data.status === "success") {
+      webStats = res.data.results;
+    } else {
+      webStats = [];
+    }
   } catch (error) {
     console.error("Error fetching data:", error);
     webStats = null; // Handle the case where fetching fails
