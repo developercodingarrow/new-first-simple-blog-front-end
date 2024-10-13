a379fb9532289c9ee7fcfeff0df1672997ba16c4
developercodingarrow <digitalcloudinfo@gmail.com>
2024-10-13 16:43:06 +0530

    admin pandel

--- src/app/_adminPanel/components/csr_components/table_elements/tableBlodtext.js
+++ src/app/_adminPanel/components/csr_components/table_elements/tableBlodtext.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./css/tableboldtext.module.css";
 export default function TableBlodtext(props) {
-  const { data } = props;
+  const { data = [] } = props;
   const truncatedData = data.length > 50 ? `${data.slice(0, 50)}...` : data;
   return (
     <div className={styles.text_wrapper}>
