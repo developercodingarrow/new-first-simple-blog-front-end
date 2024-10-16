519cf8f9697497c61b3c0325f76097ad5760fcb0
developercodingarrow <digitalcloudinfo@gmail.com>
2024-10-16 22:45:57 +0530

    check buid

--- src/app/(adminAuth)/layout.js
+++ src/app/(adminAuth)/layout.js
@@ -7,23 +7,28 @@ import ModelContextProvider from "../_contextApi/ModelContextApi";
 import TagContextProvider from "../_contextApi/TagContextApi";
 import { GOOGLE_AUTH_CLIENT_ID } from "@/config";
 import AuthContextProvider from "../_contextApi/authContext";
+import AppContextProvider from "../_contextApi/AppContext";
+import AppDrawer from "@/src/components/appDrawer/AppDrawer";
 
 export default function layout({ children }) {
   return (
     <html lang="en">
       <body>
         <AuthContextProvider>
-          <GoogleOAuthProvider clientId={GOOGLE_AUTH_CLIENT_ID}>
-            <ModelContextProvider>
-              <TagContextProvider>
-                <div>
-                  <NavBar />
-                </div>
-                <div className="children_wrapper">{children}</div>
-                <Footer />
-              </TagContextProvider>
-            </ModelContextProvider>
-          </GoogleOAuthProvider>
+          <AppContextProvider>
+            <GoogleOAuthProvider clientId={GOOGLE_AUTH_CLIENT_ID}>
+              <ModelContextProvider>
+                <TagContextProvider>
+                  <AppDrawer />
+                  <div>
+                    <NavBar />
+                  </div>
+                  <div className="children_wrapper">{children}</div>
+                  <Footer />
+                </TagContextProvider>
+              </ModelContextProvider>
+            </GoogleOAuthProvider>
+          </AppContextProvider>
         </AuthContextProvider>
       </body>
     </html>
